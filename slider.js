c6668409d251b0a48c9126b3a6196ff0e2ae3244
kumudini nimhan <kumudininimhan@gmail.com>
2017-05-22 06:08:31 +0530

    basic slider code

--- slider.js
+++ slider.js
@@ -1,7 +1,39 @@
+var images = [
+  "http://lorempixel.com/600/350/?1",
+  "http://lorempixel.com/600/350/?2",
+  "http://lorempixel.com/600/350/?3",
+  "http://lorempixel.com/600/350/?4",
+  "http://lorempixel.com/600/350/?5",
+  "http://lorempixel.com/600/350/?6",
+  "http://lorempixel.com/600/350/?7",
+  "http://lorempixel.com/600/350/?8",
+  "http://lorempixel.com/600/350/?9",
+  "http://lorempixel.com/600/350/?10",
+];
+
+var currentImageIndex;
+
+function getImage(index) {
+  currentImageIndex = index;
+  document.image.src = images[index];
+}
+
 function next() {
-  console.log('next')
+  if(currentImageIndex == 9) {
+    getImage(0);
+  } else {
+    getImage(currentImageIndex + 1);
+  }
 }
 
 function prev() {
-  console.log('prev')
+  if(currentImageIndex == 0) {
+    getImage(9);
+  } else {
+    getImage(currentImageIndex - 1);
+  }
+}
+
+function setPageNo() {
+  document.getElementById("imageIndex").innerHTML = (currentImageIndex + 1) + '/10';  
 }
